dda0de6847fd7c0f7bf7c84b0ce183839e2ed8ff
chevvi <DDR47@yandex.ru>
2016-08-26 13:06:52 +0400

    Update make_common_file_zones_from_template.py

--- make_common_file_zones_from_template.py
+++ make_common_file_zones_from_template.py
@@ -34,7 +34,7 @@ if isgoodipv4(sys.argv[1]) == True:
         text=text+str("\n\t\t%s; \t//%s" % (infraDns[ns], ns))
     f=open('text1.txt', 'a')
     f.write(text)
-    f.write('}')
+    f.write('\n\t};\n};\n')
     f.close()
 
 
